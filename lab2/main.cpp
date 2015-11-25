ac865bc4f72260881f416b29908d22386551c052
erila135 <erila135@dog13.itn.liu.se>
2015-11-25 14:12:43 +0100

    started with intersection

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -24,7 +24,7 @@ osg::ref_ptr<osg::Geometry> linesGeom;
 
 
 //-----------------------
-// function declarations 
+// function declarations
 //-----------------------
 void myInitOGLFun();
 void myPreSyncFun();
@@ -39,6 +39,7 @@ void myDecodeFun();
 void initOSG();
 void createOSGScene();
 void setupLightSource();
+void calculateIntersections();
 
 osg::Vec3d wand_start;
 osg::Vec3d wand_end;
@@ -84,18 +85,18 @@ void myInitOGLFun(){
   initOSG();
   createOSGScene();
   setupLightSource();
-  
+
   glEnable(GL_DEPTH_TEST);
 
   //only store the tracking data on the master node
   if( !gEngine->isMaster() ) return;
-  
+
   for(size_t i = 0; i < sgct::Engine::getTrackingManager()->getNumberOfTrackers(); i++){
     sgct::SGCTTracker * trackerPtr = sgct::Engine::getTrackingManager()->getTrackerPtr(i);
-    
+
     for(size_t j=0; j<trackerPtr->getNumberOfDevices(); j++){
       sgct::SGCTTrackingDevice * devicePtr = trackerPtr->getDevicePtr(j);
-      
+
       if( devicePtr->hasSensor() ){
         sharedTransforms.addVal( glm::mat4(1.0f) );
       }
@@ -104,23 +105,23 @@ void myInitOGLFun(){
 }
 
 void myPreSyncFun(){
-  
+
   // Only master does things in pre-sync; slaves return
   if( !gEngine->isMaster() ) return;
-  
+
   curr_time.setVal( sgct::Engine::getTime() );
-  
+
   std::stringstream message;
-  
+
   size_t index = 0;
   for(size_t i = 0; i < sgct::Engine::getTrackingManager()->getNumberOfTrackers(); i++){
     sgct::SGCTTracker * trackerPtr = sgct::Engine::getTrackingManager()->getTrackerPtr(i);
-    
+
     for(size_t j = 0; j < trackerPtr->getNumberOfDevices(); j++){
       sgct::SGCTTrackingDevice * devicePtr = trackerPtr->getDevicePtr(j);
 
       message << "Device " << i << " on tracker " << j << std::endl;
-      
+
       if( devicePtr->hasSensor() ){
         sharedTransforms.setValAt( index, devicePtr->getWorldTransform() );
         message << "Position:" << std::endl << "  "
@@ -133,7 +134,7 @@ void myPreSyncFun(){
                 << devicePtr->getEulerAngles().z << std::endl;
         index++;
       }
-      
+
       if( devicePtr->hasButtons() ){
         message << "Buttons:" << std::endl << "  ";
         for( int idx = 0 ; idx < devicePtr->getNumberOfButtons() ; ++idx ){
@@ -141,7 +142,7 @@ void myPreSyncFun(){
         }
         message << std::endl;
       }
-      
+
       if( devicePtr->hasAnalogs() ){
         message << "Analogs:" << std::endl << "  ";
         for( int idx = 0 ; idx < devicePtr->getNumberOfAxes() ; ++idx ){
@@ -152,7 +153,7 @@ void myPreSyncFun(){
       message << std::endl;
     }
   }
-  
+
   sharedText.setVal(message.str());
 }
 
@@ -168,16 +169,16 @@ void myPostSyncPreDrawFun(){
   // Draw wand in OSG
   if( sharedTransforms.getSize() > WAND_SENSOR_IDX ){
     wand_matrix = sharedTransforms.getValAt(WAND_SENSOR_IDX);
-    
+
     glm::vec3 wand_position = glm::vec3(wand_matrix*glm::vec4(0,0,0,1));
     //glm::quat wand_orientation = glm::quat_cast(wand_matrix);
     glm::mat3 wand_orientation = glm::mat3(wand_matrix);
-    
+
     glm::vec3 start = wand_position;
     glm::vec3 end = wand_position + wand_orientation * glm::vec3(0,0,-10);
     wand_start = osg::Vec3(start.x, start.y, start.z);
     wand_end = osg::Vec3(end.x, end.y, end.z);
-    
+
     osg::Vec3Array* vertices = new osg::Vec3Array();
     vertices->push_back(wand_start);
     vertices->push_back(wand_end);
@@ -188,20 +189,24 @@ void myPostSyncPreDrawFun(){
   if (!mViewer->done()){
     mViewer->eventTraversal();
     mViewer->updateTraversal();
+    calculateIntersections();
   }
 }
 
+void calculateIntersections() {
+}
+
 void myDrawFun() {
-  const int * curr_vp = gEngine->getActiveViewportPixelCoords();
+  const int * curr_vp = gEngine->getCurrentViewportPixelCoords();
   mViewer->getCamera()->setViewport(curr_vp[0], curr_vp[1], curr_vp[2], curr_vp[3]);
-  mViewer->getCamera()->setProjectionMatrix( osg::Matrix( glm::value_ptr(gEngine->getActiveViewProjectionMatrix() ) ));
+  mViewer->getCamera()->setProjectionMatrix( osg::Matrix( glm::value_ptr(gEngine->getCurrentViewProjectionMatrix() ) ));
 
   mViewer->renderingTraversals();
-  
+
 	// draw text with OpenGL
-	float textVerticalPos = static_cast<float>(gEngine->getActiveWindowPtr()->getYResolution()) - 100.0f;
+	float textVerticalPos = static_cast<float>(gEngine->getCurrentWindowPtr()->getYResolution()) - 100.0f;
 	int fontSize = 12;
-	
+
 	glColor3f(1.0f, 1.0f, 1.0f);
 	sgct_text::print(sgct_text::FontManager::instance()->getFont( "SGCTFont", fontSize ),
 		120.0f, textVerticalPos,
@@ -227,7 +232,7 @@ void myCleanUpFun(){
 }
 
 void keyCallback(int key, int action) {
-  
+
   if( !gEngine->isMaster() ) return;
 
   switch (key) {
@@ -260,7 +265,7 @@ void initOSG(){
     new osgViewer::GraphicsWindowEmbedded(traits.get());
 
   mViewer->getCamera()->setGraphicsContext(graphicsWindow.get());
-  
+
   //SGCT will handle the near and far planes
   mViewer->getCamera()->setComputeNearFarMode(osgUtil::CullVisitor::DO_NOT_COMPUTE_NEAR_FAR);
   mViewer->getCamera()->setClearColor( osg::Vec4( 0.0f, 0.0f, 0.0f, 0.0f) );
@@ -302,25 +307,34 @@ void createOSGScene(){
   mRootNode->addChild(createWand());
 
   osg::ref_ptr<osg::Node>            mModel;
+  osg::ref_ptr<osg::Node>            mCessnaModel;
   osg::ref_ptr<osg::MatrixTransform> mModelTrans;
+  osg::ref_ptr<osg::MatrixTransform> mCessnaTrans;
 
 
   mSceneTrans		= new osg::MatrixTransform();
   mModelTrans		= new osg::MatrixTransform();
-  
+  mCessnaTrans	= new osg::MatrixTransform();
+
   // rotate osg model to match sgct coordinate system
   mModelTrans->preMult(osg::Matrix::rotate(glm::radians(-90.0f),
                                            1.0f, 0.0f, 0.0f));
-  
+
+  mCessnaTrans->preMult(osg::Matrix::rotate(glm::radians(-90.0f),
+                                           1.0f, 0.0f, 0.0f));
   mRootNode->addChild( mSceneTrans.get() );
   mSceneTrans->addChild( mModelTrans.get() );
+  mSceneTrans->addChild( mCessnaTrans.get() );
 
   sgct::MessageHandler::instance()->print("Loading model airplane.ive'...\n");
   mModel = osgDB::readNodeFile("airplane.ive");
+  mCessnaModel = osgDB::readNodeFile("cessna.osg");
 
-  if ( mModel.valid() ){
+  if ( mModel.valid() && mCessnaModel.valid()){
     sgct::MessageHandler::instance()->print("Model loaded successfully!\n");
+
     mModelTrans->addChild(mModel.get());
+    mCessnaTrans->addChild(mCessnaModel.get());
 
     //get the bounding box
     osg::ComputeBoundsVisitor cbv;
@@ -329,6 +343,7 @@ void createOSGScene(){
 
     osg::Vec3f tmpVec;
     tmpVec = bb.center();
+    tmpVec.x() += 20;
 
     // translate model center to origin
     mModelTrans->postMult(osg::Matrix::translate( -tmpVec ) );
@@ -340,13 +355,28 @@ void createOSGScene(){
     sgct::MessageHandler::instance()->print("Model bounding sphere center:\tx=%f\ty=%f\tz=%f\n", tmpVec[0], tmpVec[1], tmpVec[2] );
     sgct::MessageHandler::instance()->print("Model bounding sphere radius:\t%f\n", bb.radius() );
 
+    //same as above for cessna
+    mCessnaModel->accept( cbv );
+
+    tmpVec = bb.center();
+    tmpVec.x() -= 20;
+    // translate model center to origin
+    mCessnaTrans->postMult(osg::Matrix::translate( -tmpVec ) );
+
+    // scale model to a manageable size
+    scale = 0.1 / bb.radius();
+    mCessnaTrans->postMult(osg::Matrix::scale( scale, scale, scale ));
+
     //disable face culling
+    mCessnaModel->getOrCreateStateSet()->setMode( GL_CULL_FACE,
+
+                                            osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
     mModel->getOrCreateStateSet()->setMode( GL_CULL_FACE,
                                             osg::StateAttribute::OFF | osg::StateAttribute::OVERRIDE);
   }
   else
     sgct::MessageHandler::instance()->print("Failed to read model!\n");
-  
+
 }
 
 void setupLightSource(){
